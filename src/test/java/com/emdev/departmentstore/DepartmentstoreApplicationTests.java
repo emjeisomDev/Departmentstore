726c788a40195f0bf5dabc7bbce120690c348cac
Emanuel Jeisom <emjeisom.dev@gmail.com>
2026-03-15 15:34:17 -0300

    Project Created

---
+++ src/test/java/com/emdev/departmentstore/DepartmentstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emdev.departmentstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
